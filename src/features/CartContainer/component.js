661fea2f462189c3e6c2d1ca3422ecbed3683501
pmita <panos.mita2@outlook.com>
2021-10-13 18:00:07 +0100

    Fix/CartContainer UI re-vamp

--- src/features/CartContainer/component.js
+++ src/features/CartContainer/component.js
@@ -11,23 +11,31 @@ const CartContainer = () => {
         <>
             <h1>Shopping Cart</h1>
             <section className='cartSection'>
-                <div className='cartSection_products'>
-                    {cart.map(cartItem => {
-                        return (
-                            <CartItem 
-                                key={cartItem.id}
-                                product={cartItem} 
-                            />
-                        );
-                    })}
-                </div>
-                <div className='cartSection_price'>
-                    <h2>Apply Discount Code</h2>
-                    <h4>Subtotal 1000 $</h4>
-                    <h4>Tax</h4>
-                    <h2>Order Total</h2>
-                    <button className='btnAction'>PROCEED TO CHECKOUT</button>
-                </div>
+                    <aside className='cartSection_products'>
+                        {cart.map(cartItem => {
+                            return (
+                                <CartItem 
+                                    key={cartItem.id}
+                                    product={cartItem} 
+                                />
+                            );
+                        })}
+                    </aside>
+                    <aside className='cartSection_details'>
+                        <div className='cartSection_discounts'>
+                            <h2>Apply Discount Code</h2>
+                            <form>
+                                <input type='text' value='Enter discount code' />
+                                <button>APPLY DISCOUNT</button>
+                            </form>
+                        </div>
+                        <div className='cartSection_price'>
+                            <h4>Subtotal 1000 $</h4>
+                            <h4>Tax</h4>
+                            <h2>Order Total</h2>
+                            <button className='btnAction'>PROCEED TO CHECKOUT</button>
+                        </div>
+                    </aside>
             </section> 
         </>
     );
